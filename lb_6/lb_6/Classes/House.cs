7c46ce191f2569afd8f7ac6c73261fb5b673fecf
astymic <max1clash2coc@gmail.com>
2025-04-08 13:48:29 +0200

    Changed

---
+++ lb_6/lb_6/Classes/House.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lb_6.Classes
+{
+    class House : RealEstate
+    {
+        public double GardenSize { get; set; }
+        public bool Pool { get; set; }
+
+        public House()
+        {
+            GardenSize = 0;
+            Pool = false;
+        }
+
+        public House(double gardenSize, bool pool)
+        {
+            if (GardenSize < 0) throw new ValueLessThanZero("Garden size");
+            GardenSize = gardenSize;
+            Pool = pool;
+        }
+
+        public House(string name, decimal price, string location, double size, string type, double gardenSize, bool pool)
+            : base(name, price, location, size, type)
+        {
+            if (GardenSize < 0) throw new ValueLessThanZero("Garden size");
+            GardenSize = gardenSize;
+            Pool = pool;
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}, Garden Size: {GardenSize}, {(Pool ? "There is" : "No")} Pool";
+        }
+    }
+}
