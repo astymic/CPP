005397ad88a21e516e23de5d4b80bff4f09a804c
astymic <max1clash2coc@gmail.com>
2025-05-16 14:45:15 +0200

    IRT start

---
+++ IRT/IRT/Classes/RealEstateInvestment.cs
@@ -0,0 +1,69 @@
+﻿using IRT.Interfaces;
+
+namespace IRT.Classes
+{
+    class RealEstateInvestment : Product, IName<RealEstateInvestment>, ICustomSerializable
+    {
+        public string Location { get; set; }
+        public decimal MarketValue { get; set; }
+        public string InvestmentType { get; set; }
+
+        public RealEstateInvestment()
+        {
+            Location = string.Empty;
+            MarketValue = 0;
+            InvestmentType = string.Empty;
+        }
+
+        public RealEstateInvestment(string location, decimal marketValue)
+        {
+            if (marketValue <= 0) throw new ValueLessThanZero("Market value");
+            Location = location;
+            MarketValue = marketValue;
+            InvestmentType = string.Empty;
+        }
+
+        public RealEstateInvestment(string location, decimal marketValue, string investmentType)
+        {
+            if (marketValue <= 0) throw new ValueLessThanZero("Market value");
+            Location = location;
+            MarketValue = marketValue;
+            InvestmentType = investmentType;
+        }
+
+        public RealEstateInvestment(string name, decimal price, string location, decimal marketValue, string investmentType)
+            : base(name, price)
+        {
+            if (marketValue <= 0) throw new ValueLessThanZero("Market value");
+            Location = location;
+            MarketValue = marketValue;
+            InvestmentType = investmentType;
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}, Location: {Location}, Market Value: {MarketValue}, Invenstment Type: {InvestmentType}";
+        }
+
+        public void Serialize(BinaryWriter writer)
+        {
+            writer.Write(Name);
+            writer.Write(Price);
+            writer.Write(Location);
+            writer.Write(MarketValue);
+            writer.Write(InvestmentType);
+        }
+
+        public static RealEstateInvestment Deserialize(BinaryReader reader)
+        {
+            return new RealEstateInvestment
+            {
+                Name = reader.ReadString(),
+                Price = reader.ReadDecimal(),
+                Location = reader.ReadString(),
+                MarketValue = reader.ReadDecimal(),
+                InvestmentType = reader.ReadString()
+            };
+        }
+    }
+}
---
+++ IRT/IRT/Interfaces/IName.cs
@@ -0,0 +1,14 @@
+﻿namespace IRT.Interfaces
+{
+    public interface IName : IComparable
+    {
+        string Name { get; set; }
+        decimal Price { get; set; }
+
+        int CompareTo(object obj);
+    }
+    public interface IName<T>
+    {
+        string Name { get; set; }
+    }
+}
